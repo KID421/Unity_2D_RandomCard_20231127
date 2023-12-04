43e87dbe4399794786826d34a808202b4d25f69b
KID <50666207+KID421@users.noreply.github.com>
2023-12-04 20:44:05 +0800

    Update 添加卡片資料

--- Assets/RandomCard.cs
+++ Assets/RandomCard.cs
@@ -1,18 +1,46 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 using UnityEngine.UI;
 
+[DefaultExecutionOrder(100)]
 public class RandomCard : MonoBehaviour
 {
+    public static RandomCard instance;
+
     [SerializeField, Header("所有日記圖片")]
-    private Sprite[] pictures;
+    public Sprite[] pictures;
 
     [SerializeField, Header("是否拿到日記圖片")]
-    private bool[] picturesState;
+    public bool[] picturesState;
 
     private Image imgPicture;
 
-
     private void Awake()
+    {
+        instance = this;
+
+        GetCardIsGetInformation();
+
+        imgPicture = GameObject.Find("圖片").GetComponent<Image>();
+
+        GetRandomPicture();                                                         // 開始遊戲時取得隨機圖片
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            for (int i = 0; i < picturesState.Length; i++)
+            {
+                PlayerPrefs.SetString("圖片" + i, "未拿到");
+            }
+
+            GetCardIsGetInformation();
+        }
+    }
+
+    [ContextMenu("取得所有卡片是否獲得")]
+    private void  GetCardIsGetInformation()
     {
         picturesState = new bool[pictures.Length];                                  // 新增全部的圖片狀態陣列 有幾張圖就有幾個狀態
 
@@ -20,15 +48,20 @@ public class RandomCard : MonoBehaviour
         {
             picturesState[i] = PlayerPrefs.GetString("圖片" + i) == "已拿到";
         }
-
-        imgPicture = GameObject.Find("圖片").GetComponent<Image>();
-
-        GetRandomPicture();                                                         // 開始遊戲時取得隨機圖片
     }
 
     private void GetRandomPicture()
     {
         int indexRandom = Random.Range(0, pictures.Length);                         // 隨機獲得一張圖片
+
+        // 拿狀態內還沒有獲得的卡片數量
+        var pictureStateFalse = picturesState.Where(x => x == false);
+        // 如果全部卡片都拿過 就 跳出
+        if (pictureStateFalse.Count() == 0)
+        {
+            print("全部都拿過了");
+            return;
+        }
 
         while (PlayerPrefs.GetString("圖片" + indexRandom) == "已拿到")              // 不重複處理 while (true) { 程式 } - 當 () 布林值為 true 會持續執行
         {
---
+++ Assets/ShowCard.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[DefaultExecutionOrder(200)]
+public class ShowCard : MonoBehaviour
+{
+    [SerializeField, Header("卡片")]
+    private Image[] imgCards;
+
+    private void Awake()
+    {
+        Show();
+    }
+
+    private void Show()
+    {
+        for (int i = 0; i < imgCards.Length; i++)
+        {
+            bool isGet = RandomCard.instance.picturesState[i];
+            imgCards[i].sprite = isGet ? RandomCard.instance.pictures[i] : null;
+            print(1);
+        }
+    }
+}
